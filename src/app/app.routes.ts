519554b6fd6434437cb50795f919e69873aef79b
Te-Jung Chen <tchen342@berkeley.edu>
2025-02-18 20:13:13 -0800

    routing and base code structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { UserinfoComponent } from './pages/userinfo/userinfo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'userinfo', component: UserinfoComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
+
+export default routes;
